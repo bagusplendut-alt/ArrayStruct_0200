790cad0a810a6ce0b6112ed90dfd526f1973dad1
bagusplendut-alt <bagusplendut@gmail.com>
2026-04-06 15:13:22 +0700

    input data index 1 sampai 4

--- Array.cpp
+++ Array.cpp
@@ -11,4 +11,12 @@ int main(){
 
     cout << "Data index 0 = ";
     cin >> kamu[0];
+    cout << "Data index 1 = ";
+    cin >> kamu[1];
+    cout << "Data index 2 = ";
+    cin >> kamu[2];
+    cout << "Data index 3 = ";
+    cin >> kamu[3];
+    cout << "Data index 4 = ";
+    cin >> kamu[4];
 }
