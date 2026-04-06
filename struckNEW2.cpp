ed6aa87f825ee977978f8a4ebbe5be70401137fa
bagusplendut-alt <bagusplendut@gmail.com>
2026-04-06 15:37:26 +0700

    library , struck, menambahkan input dan output

---
+++ struckNEW2.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+struct detailAlamat
+{
+    string desa;
+    string kota;
+};
+
+struct Orang
+{
+    string nama;
+    detailAlamat alamat;
+    int umur;
+};
+
+int main() {
+    Orang mhs;
+    cout << "Nama : ";
+    cin >> mhs.nama;
+    cout << "Alamat Desa : ";
+    cin >> mhs.alamat.desa;
+    cout << "Alamat Kota : ";
+    cin >> mhs.alamat.kota;
+    cout << "Umur : ";
+    cin >> mhs.umur;
+
+    cout << endl;
+    // tampilkan data
+    cout << "Nama : " << mhs.nama << endl;
+    cout << "Desa : " << mhs.alamat.desa << endl;
+    cout << "Kota : " << mhs.alamat.kota << endl;
+    cout << "Umur : " << mhs.umur << endl;
+
+    return 0;
+}
