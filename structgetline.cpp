40741ab4b45dbe82dd4a0e62f0e99b91ea6aba0a
bagusplendut-alt <bagusplendut@gmail.com>
2026-04-06 15:45:14 +0700

    input getline dan tampilan

--- structgetline.cpp
+++ structgetline.cpp
@@ -13,3 +13,22 @@ struct Orang
     detailAlamat alamat;
     int umur;
 };
+
+int main() {
+    Orang mhs;
+    cout << "Nama : ";
+    getline(cin, mhs.nama);
+    cout << "Alamat Desa : ";
+    getline(cin, mhs.alamat.desa);
+    cout << "Alamat Kota : ";
+    getline(cin, mhs.alamat.kota);
+    cout << "Umur : ";
+    cin >> mhs.umur;
+
+    cout << endl;
+    // tampilkan
+    cout << " Nama : " << mhs.nama << endl;
+    cout << " Desa : " << mhs.alamat.desa << endl;
+    cout << " Kota : " << mhs.alamat.kota << endl;
+    cout << " Umur : " << mhs.umur << endl;
+}
